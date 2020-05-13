232e40e692dc865a61789600a6bd5456efab0931
theovast <theovast@MacBook-Pro-de-theovast-2.local>
2020-05-14 01:07:06 +0200

    ini

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
@@ -17,6 +17,13 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }, {
+    path: '/portfolio',
+    name: 'portfolio',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/dashboard.vue')
   }
 ]
 
