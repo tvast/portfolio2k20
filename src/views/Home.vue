daad7bacb1523133fbc689fda765843a7152aab9
theovast <theovast@MacBook-Pro-de-theovast-2.local>
2020-05-14 02:12:52 +0200

    first draft

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,218 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Portoflio Théophile vast</h1>
+
+    <div id="app" class="container">
+      <card
+        data-image="https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1602&q=80"
+      >
+        <h1 slot="header">Dashboard</h1>
+        <p slot="content">Building simple interface for complex data flow</p>
+      </card>
+      <card
+        data-image="https://images.unsplash.com/photo-1479659929431-4342107adfc1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop="
+      >
+        <h1 slot="header">Beaches</h1>
+        <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </card>
+      <card
+        data-image="https://images.unsplash.com/photo-1479644025832-60dabb8be2a1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop="
+      >
+        <h1 slot="header">Trees</h1>
+        <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </card>
+      <card
+        data-image="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+      >
+        <h1 slot="header">Lakes</h1>
+        <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </card>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
+import card from "@/components/card.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
+    card
   }
-}
+};
 </script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Playfair+Display&family=Poiret+One&display=swap");
+
+// body {
+//   margin: 40px 0;
+//   font-family: "Playfair Display", serif;
+//   font-size: 14px;
+//   font-weight: 500;
+//   background-color: #bcaaa4;
+//   -webkit-font-smoothing: antialiased;
+// }
+
+// .title {
+//   font-family: "Poiret One", cursive;
+//   font-size: 24px;
+//   font-weight: 700;
+//   color: #5d4037;
+//   text-align: center;
+// }
+
+p {
+  line-height: 1.5em;
+}
+
+h1 + p,
+p + p {
+  margin-top: 10px;
+}
+
+.container {
+  padding: 40px 80px;
+  display: -webkit-box;
+  display: flex;
+  flex-wrap: wrap;
+  -webkit-box-pack: center;
+  justify-content: center;
+}
+
+.card-wrap {
+  margin: 10px;
+  -webkit-transform: perspective(800px);
+  transform: perspective(800px);
+  -webkit-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+  cursor: pointer;
+}
+.card-wrap:hover .card-info {
+  -webkit-transform: translateY(0);
+  transform: translateY(0);
+}
+.card-wrap:hover .card-info p {
+  opacity: 1;
+}
+.card-wrap:hover .card-info,
+.card-wrap:hover .card-info p {
+  -webkit-transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+  transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+}
+.card-wrap:hover .card-info:after {
+  -webkit-transition: 5s cubic-bezier(0.23, 1, 0.32, 1);
+  transition: 5s cubic-bezier(0.23, 1, 0.32, 1);
+  opacity: 1;
+  -webkit-transform: translateY(0);
+  transform: translateY(0);
+}
+.card-wrap:hover .card-bg {
+  -webkit-transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1),
+    opacity 5s cubic-bezier(0.23, 1, 0.32, 1);
+  transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1),
+    opacity 5s cubic-bezier(0.23, 1, 0.32, 1);
+  opacity: 0.8;
+}
+.card-wrap:hover .card {
+  -webkit-transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1),
+    box-shadow 2s cubic-bezier(0.23, 1, 0.32, 1);
+  transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1),
+    box-shadow 2s cubic-bezier(0.23, 1, 0.32, 1);
+  box-shadow: rgba(255, 255, 255, 0.2) 0 0 40px 5px, white 0 0 0 1px,
+    rgba(0, 0, 0, 0.66) 0 30px 60px 0, inset #333 0 0 0 5px,
+    inset white 0 0 0 6px;
+}
+
+.card {
+  position: relative;
+  -webkit-box-flex: 0;
+  flex: 0 0 240px;
+  width: 240px;
+  height: 320px;
+  background-color: #333;
+  overflow: hidden;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.66) 0 30px 60px 0, inset #333 0 0 0 5px,
+    inset rgba(255, 255, 255, 0.5) 0 0 0 6px;
+  -webkit-transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+  transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+}
+
+.card-bg {
+  opacity: 0.5;
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  -webkit-transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95),
+    opacity 5s 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+  transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95),
+    opacity 5s 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+  pointer-events: none;
+}
+
+.card-info {
+  padding: 20px;
+  position: absolute;
+  bottom: 0;
+  color: #fff;
+  -webkit-transform: translateY(40%);
+  transform: translateY(40%);
+  -webkit-transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
+  transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
+}
+.card-info p {
+  opacity: 0;
+  text-shadow: black 0 2px 3px;
+  -webkit-transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
+  transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
+}
+.card-info * {
+  position: relative;
+  z-index: 1;
+}
+.card-info:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  background-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(transparent),
+    to(rgba(0, 0, 0, 0.6))
+  );
+  background-image: linear-gradient(
+    to bottom,
+    transparent 0%,
+    rgba(0, 0, 0, 0.6) 100%
+  );
+  background-blend-mode: overlay;
+  opacity: 0;
+  -webkit-transform: translateY(100%);
+  transform: translateY(100%);
+  -webkit-transition: 5s 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+  transition: 5s 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+}
+
+.card-info h1 {
+  font-family: "Poiret One", cursive;
+  font-size: 36px;
+  font-weight: 700;
+  text-shadow: rgba(0, 0, 0, 0.5) 0 10px 10px;
+}
+</style>
---
+++ src/views/dashboard.vue
@@ -0,0 +1,172 @@
+<template>
+  <!-- <div class="home"> -->
+  <grid-layout
+    :layout.sync="layout"
+    :col-num="12"
+    :row-height="30"
+    :is-draggable="true"
+    :is-resizable="true"
+    :is-mirrored="false"
+    :vertical-compact="true"
+    :margin="[10, 10]"
+    :use-css-transforms="true"
+  >
+    <grid-item
+      v-for="item in layout"
+      :x="item.x"
+      :y="item.y"
+      :w="item.w"
+      :h="item.h"
+      :i="item.i"
+      :key="item.i"
+    >{{item.i.kpi}}</grid-item>
+  </grid-layout>
+  <!-- </div> -->
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import VueGridLayout from "vue-grid-layout";
+
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      layout: [
+        {
+          x: 0,
+          y: 0,
+          w: 2,
+          h: 4,
+          i: { title: "kpi", kpi: "test", color: "green" }
+        },
+        {
+          x: 2,
+          y: 0,
+          w: 2,
+          h: 4,
+          i: { title: "kpi2", kpi: "1000", color: "green" }
+        },
+        {
+          x: 4,
+          y: 0,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 6,
+          y: 0,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 8,
+          y: 0,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 10,
+          y: 0,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 0,
+          y: 5,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 2,
+          y: 5,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 4,
+          y: 5,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 6,
+          y: 5,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 8,
+          y: 5,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 10,
+          y: 5,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 0,
+          y: 10,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 2,
+          y: 10,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 4,
+          y: 10,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 6,
+          y: 10,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 8,
+          y: 10,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        },
+        {
+          x: 10,
+          y: 10,
+          w: 2,
+          h: 4,
+          i: { title: "kpi3", kpi: "2400", color: "green" }
+        }
+      ]
+    };
+  },
+  components: {
+    // HelloWorld,
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem
+  }
+};
+</script>
